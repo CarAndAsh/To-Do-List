c0c3c99b50468effe6fb4d8209f4f6d8351486ca
CarAndAsh <zeixes@gmail.com>
2023-05-08 15:34:57 +0300

    Add macros for generating forms. Add errorhandler for 404 and template for it. Add for next correct formf for login abd registration.

--- app.py
+++ app.py
@@ -1,43 +1,59 @@
 from flask import Flask, render_template, request
-from flask_bootstrap.static import css, js, fonts
 from flask_bootstrap import Bootstrap
-from forms import TaskCerator, Task, TaskList, TLGroup
+from forms import Task, TaskList, TLGroup, TaskCreator, LoginForm, RegistrationForm
 
 # TODO realize classes for task, list, group of lists and m.b. app
 # TODO every list or group of lists create link on nav-block by '+'
 # TODO list looks like name on page-name in representation-parameters-view of tasks
 # TODO task looks like check-text-edit-delete-catch
 # TODO m.b.realize steps in task looks like task
-# work time: 4 h
+# work time: 10 h
 
 app = Flask(__name__)
 b_app = Bootstrap(app)
+app.config['SECRET_KEY'] = 'qwertyfasdqwerty!@#$654654'
 
-task_text = []
 
-
-@app.route('/<username>')
+# @app.route('/<username>')
 @app.route('/', methods=['GET', 'POST'])
 def main_page(username=None):
-    global task_text
-    if request.method == 'POST':
-        req = request.form.get(key='task')
-        if req == 'clr':
-            task_text = []
-        else:
-            task_text.append(req)
+    task_text = ''
     context = {'title': 'To-Do List',
-               # 'taskcreator': TaskCerator(),
-               # 'task': Task(),
+               'task': Task(),
+               'task_creator': TaskCreator(),
+
                # 'tasklist': TaskList(),
                # 'tlgroup': TLGroup(),
-               'task_text': task_text}
+               }
+    if request.method == 'POST':
+        req = request.get_data()
+        print(req)
+
     return render_template('main.html', **context)
 
 
 @app.route('/info')
 def info_page():
     return render_template('info.html', title='Info')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    context = {'title': 'Login',
+               'log_form': LoginForm()}
+    return render_template('login.html', **context)
+
+
+@app.route('/registraton', methods=['GET', 'POST'])
+def reg_page():
+    context = {'title': 'Registration',
+               'reg_form': RegistrationForm()}
+    return render_template('new_user_reg.html', **context)
+
+
+@app.errorhandler(404)
+def page_404(error):
+    return render_template('page_404.html', title='Not found')
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,28 +1,34 @@
-import flask_wtf
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import InputRequired
 
 
-class TaskCerator(FlaskForm):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        entry = StringField("Введите текст задачи...")
-        btn = SubmitField('+')
+class TaskCreator(FlaskForm):
+    task_text = StringField('Введите текст задачи: ', validators=[InputRequired()])
+    submit = SubmitField('add')
 
 
 class Task(FlaskForm):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-task_list = {name: Task for name in ('сделать', 'прочесть', 'написать', 'отправить')}
+    check = BooleanField(label='')
+    task_label = StringField(label='')
 
 
 class TaskList(FlaskForm):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    pass
 
 
 class TLGroup:
-    def __init__(self):
-        pass
+    pass
+
+
+class LoginForm(FlaskForm):
+    login = StringField()
+    password = PasswordField()
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    login = StringField()
+    new_password = PasswordField()
+    repeat_password = PasswordField()
+    submit = SubmitField('Зарегистрироваться')
